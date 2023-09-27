3d7badb5f49fdb2d0b20422cf03c20d806e6fbbb
tony-brian <btoni3823@gmail.com>
2023-09-27 22:34:59 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -13,8 +13,14 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+
 @app.route('/')
 def home():
+    """
+    Handle requests to the homepage.
+    Returns:
+        Response: A Flask Response object with a welcome message.
+    """
     return '<h1>Zoo app</h1>'
 
 @app.route('/animal/<int:id>')
@@ -24,11 +30,77 @@ def animal_by_id(id):
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
     return ''
+@app.route('/animal/<int:num>')
+def animal_by_id(num):
+    """
+    Handle requests to display information for an animal by its ID.
+    Args:
+        num (int): The ID of the animal to display.
+    Returns:
+        Response: A Flask Response object with animal information.
+    """
+    animal = Animal.query.filter(Animal.id == num).first()
+
+    response_body = f"""
+        <ul>ID: {animal.id}</ul>
+        <ul>Name: {animal.name}</ul>
+        <ul>Species: {animal.species}</ul>
+        <ul>Zookeeper: {animal.zookeeper.name}</ul>
+        <ul>Enclosure: {animal.enclosure.environment}</ul>    
+    """
+    return make_response(response_body, 200)
+
+
+@app.route('/zookeeper/<int:num>')
+def zookeeper_by_id(num):
+    """
+    Handle requests to display information for a zookeeper by their ID.
+    Args:
+        num (int): The ID of the zookeeper to display.
+    Returns:
+        Response: A Flask Response object with zookeeper information.
+    """
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == num).first()
+
+    animal_names_html = ""
+    for animal in zookeeper.animals:
+        animal_names_html += f"<ul>Animal: {animal.name}</ul>"
+
+    response_body = f"""
+        <ul>ID: {zookeeper.id}</ul>
+        <ul>Name: {zookeeper.name}</ul>
+        <ul>Birthday: {zookeeper.birthday}</ul>
+        {animal_names_html}
+    """
+
+    return make_response(response_body, 200)
+
+
+@app.route('/enclosure/<int:num>')
+def enclosure_by_id(num):
+    """
+    Handle requests to display information for an enclosure by its ID.
+    Args:
+        num (int): The ID of the enclosure to display.
+    Returns:
+        Response: A Flask Response object with enclosure information.
+    """
+    enclosure = Enclosure.query.filter(Enclosure.id == num).first()
+
+    animal_names_html = ""
+    for animal in enclosure.animals:
+        animal_names_html += f"<ul>Animal: {animal.name}</ul>"
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
     return ''
-
+    response_body = f"""
+        <ul>ID: {enclosure.id}</ul>
+        <ul>Environment: {enclosure.environment}</ul>
+        <ul>Open to Visitors: {enclosure.open_to_visitors}</ul>
+        {animal_names_html}
+    """
+    return make_response(response_body, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -11,13 +11,19 @@ class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    birthday = db.Column(db.Date, nullable=False)
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String(80), nullable=False)
+    open_to_visitors = db.Column(db.Boolean, nullable=False)
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    species = db.Column(db.String(80), nullable=False)
